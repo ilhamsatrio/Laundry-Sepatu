b957e0c40908ebd6f4926359a84fd844b0f5e380
ilhamsatrio <77287688+ilhamsatrio@users.noreply.github.com>
2021-03-31 09:48:14 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,69 @@
+<?php include 'header.php'; ?>
+
+<?php
+include 'koneksi.php';
+?>
+
+<!-- About -->
+<body>
+    <section class="about" id="about">
+    <div class="panel">
+        <div class="panel-heading">
+
+    <div class="panel-body">         
+      <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+               
+        <h2 class="text-center">About Us</h2>
+        <hr style="text-align:left">
+    </div>
+</div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="col-sm-3">
+                    <img src="assets/logo/spokat.png" width="300" height="300">  
+                </div>
+                <div class="col-sm-9">
+                    <p align="justify">Spokat wash adalah usaha yang bergerak dibidang jasa pencucian sepatu. Spokat wash didirikan pada tahun 2016 di daerah Utan Kayu Jakarta Timur.Sejak awal berdiri, Spokat wash sudah menjadi perhatian bagi semua orang dikarenakan usaha laundry sepatu ini yang masih terdengar asing namun menjanjikan keyakinan itulah Spokat wash mengembangkan usaha yang dimulai dari kontrakan kecil sebagai workshop dan sekarang sudah mempunyai beberapa tempat kerja sama untuk drop sepatu.Berangkat dari .Kemudian jasa yang ditawarkan mengingat banyaknya permintaan dari pelanggan kini menjadi berkembang yang sekarang terdapat jasa repaint, art, care dan bahkan bukan hanya sepatu yang dapat digarap, sekarang Spokat wash juga menerima permintaan seperti jaket dan topi.</p>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+        </div>
+    </div>
+        </div>
+</body>
+</selection>
+<!-- Akhir Dari About -->
+
+<!-- contact us -->
+
+<body>
+    <section class="contact" id="contact">
+    <div class="panel">
+        <div class="panel-heading">
+
+    <div class="panel-body">         
+      <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+               
+        <h2 class="text-center">Contact Us</h2>
+        <hr style="width:50%;text-align:left;margin-left:25%">
+    </div>
+</div>
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4 text-center">
+            <p>Petukangan Utara Jl.Haji Radin, Gg.Perjuangan 2,No.65B, Rt.12, Rw.03,Kel.Petukangan Utara,Kec. Pesanggrahan,Jakarta Selatan.Phone:+62856-9292-2176 Email: SpokatWash@gmail.com</p>
+          </div>
+        </div>
+    </div>
+        </div>
+    </div>
+        </div>
+</body>
+</selection>
+
+<!-- akhir dari contact us -->
